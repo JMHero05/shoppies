dcdd09c98d5c888348540ede681f65b848d0dd6a
Jake Mills <jake.mills.05@gmail.com>
2021-01-11 18:20:21 -0500

    Added poster styling. Added Nominate button and styling

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -5,11 +5,16 @@ export function Movie(props) {
   const { movie } = props;
 
   return (
-    <div className='movie'>
-      <img className='poster' src={movie.Poster} alt={movie.Title} />
-      <span>
-        {movie.Title} - ({movie.Year})
-      </span>
-    </div>
+    <>
+      <div className='movie'>
+        <img className='poster' src={movie.Poster} alt={movie.Title} />
+        <div className='title'>
+          {movie.Title} ({movie.Year})
+        </div>
+        <div>
+          <button className='nominate'>Nominate</button>
+        </div>
+      </div>
+    </>
   );
 }
