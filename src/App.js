eca734f1b0f7096940851a42052391dba2b36de0
Jake Mills <jake.mills.05@gmail.com>
2021-01-11 15:19:52 -0500

    Created get request to get data from OMDB, added .env to hide API key, and added .env to .gitignore

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(searchTerm) {
+    fetch(
+      `http://www.omdbapi.com/?s=${searchTerm}&page=1-2&apikey=${process.env.REACT_APP_OMDB_API_KEY}`
+    )
+      .then(async (response) => {
+        const data = await response.json();
+        if (!response.ok) {
+          const error = (data && data.message) || response.statusText;
+          return Promise.reject(error);
+        }
+        this.setState({ data: data });
+      })
+      .catch((error) => {
+        this.setState({ errorMessage: error.toString() });
+        console.error('There was an error!', error);
+      });
+  }
+
   render() {
     return <div className='App'></div>;
   }
