986628f60b54928a8432976dc10d65bb520d67d8
Jake Mills <jake.mills.05@gmail.com>
2021-01-17 15:19:09 -0500

    Styled Search bar

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import '../styles/search.css';
 
 export function SearchForm(props) {
   const [searchTerm, setSearchTerm] = useState('');
@@ -10,16 +13,15 @@ export function SearchForm(props) {
 
   return (
     <form onSubmit={submitSearchTerm}>
-      <label>
-        Search:{' '}
-        <input
-          type='text'
-          value={searchTerm}
-          placeholder='Enter a Movie Title'
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </label>
-      <input type='submit' value='Submit' />
+      <input
+        type='text'
+        value={searchTerm}
+        placeholder='Enter a Movie Title'
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button type='submit' value='Submit'>
+        <FontAwesomeIcon icon={faSearch} />
+      </button>
     </form>
   );
 }
